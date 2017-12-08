ee0b8de3672a5958eeffa97005178542f4a94008
danielarnarsson <danielar216@nemi.tskoli.is>
2017-12-08 10:37:40 +0000

    Tried to change from list to dictionary
    forgot that dictionaries are unordered

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -1,110 +1,72 @@
 #Daníel Arnarsson
 #22.11.2017
-
+import collections
 #workplace
 #make input values input("")?
 
 
 class workplace:
     def __init__(self):
-        employeeInfo = {}
+        employeeInfo = collections.OrderedDict()
         self.employeeInfo = employeeInfo
+
 
     def create_employee(self, fyrsta_nafn, sidasta_nafn, starfsgrein, laun):
         nafn = str(fyrsta_nafn + " " + sidasta_nafn)
         id = len(self.employeeInfo) + 1
         nafnOgId = (nafn, id)
-        self.employeeInfo[nafnOgId] = [id, fyrsta_nafn, sidasta_nafn, starfsgrein, laun]
+        employee = collections.OrderedDict()
+        employeeAttributes = collections.OrderedDict({"id": id, "fyrsta nafn": fyrsta_nafn, "sidasta nafn": sidasta_nafn, "starfsgrein": starfsgrein, "laun": laun})
+        employee.update({nafnOgId:employeeAttributes})
+        self.employeeInfo.update(employee)
 
 
     def search_employee(self, name=None, id=None):
         fjoldiStarfsmanna = len(self.employeeInfo)
+        employeeInfo = self.employeeInfo
         teljari = 0
         success = False
         try:
-            for key, value in self.employeeInfo.items(): #key er key, value er listinn um employee
-                teljari += 1
-                if name is not None and id is not None:
-                    if name in key and id in key:
-                        x=0
-                        for v in value:
-                            x += 1
-                            if x == 1:
-                                print("Employee ID", str(v) + ":")
-                            elif x == 2:
-                                print(" First name: ", v)
-                            elif x == 3:
-                                print(" Last name:", v)
-                            elif x == 4:
-                                print(" Position:", v)
-                            elif x == 5:
-                                print(" Salary:", v)
-                        success = True
-                        print()
-
-                    elif teljari == fjoldiStarfsmanna and success == False:
-                        print("Error: id and name didn't match any key.")
-                    else:
-                        pass
-
-                elif name in key and id is None:
+            for key, value in employeeInfo.items():
+                if name is not None and id is None:
                     if name in key:
-                        x=0
-                        for v in value:
-                            x += 1
-                            if x == 1:
-                                print("Employee ID", str(v) + ":")
-                            elif x == 2:
-                                print(" First name: ", v)
-                            elif x == 3:
-                                print(" Last name:", v)
-                            elif x == 4:
-                                print(" Position:", v)
-                            elif x == 5:
-                                print(" Salary:", v)
-                        success = True
-                        print()
-
-                    elif teljari == fjoldiStarfsmanna and success == False:
-                        print("Error: name didn't match any key.")
+                        for v, k in value.items():
+                            teljari += 1
+                            if teljari == 1:
+                                employeeid = value.get("id")
+                                print("Employee ID:", employeeid)
+                            else:
+                                print(" ", v + ":", k)
                     else:
-                        pass
+                        print("Name not in key")
 
-                elif id in key and name is None:
+                elif name is None and id is not None:
                     if id in key:
-                        x=0
-                        for v in value:
-                            x += 1
-                            if x == 1:
-                                print("Employee ID", str(v) + ":")
-                            elif x == 2:
-                                print(" First name: ", v)
-                            elif x == 3:
-                                print(" Last name:", v)
-                            elif x == 4:
-                                print(" Position:", v)
-                            elif x == 5:
-                                print(" Salary:", v)
-                        success = True
-                        print()
-
-                    elif teljari == fjoldiStarfsmanna and success == False:
-                        print("Error: id didn't match any key.")
+                        for v, k in value.items():
+                            teljari += 1
+                            if teljari == 1:
+                                employeeid = value.get("id")
+                                print("Employee ID:", employeeid)
+                            else:
+                                print(" ", v + ":", k)
                     else:
-                        pass
-
-                elif id not in key and name is None:
-                    if teljari == fjoldiStarfsmanna:
                         print("ID not in any key")
-                        print()
 
-                elif name not in key and id is None:
-                    if teljari == fjoldiStarfsmanna:
-                        print("Name not in any key")
-                        print()
+                elif name is not None and id is not None:
+                    if name in key and id in key:
+                        for v,k in value.items():
+                            teljari += 1
+                            if teljari == 1:
+                                employeeid = value.get("id")
+                                print("Employee ID:", employeeid)
+                            else:
+                                print(" ", v + ":", k)
+                    else:
+                        print("name and ID did not match any key")
 
         except NameError as error:
             print(error)
+
 
     def delete_employee(self, name=None, id=None):
         fjoldiStarfsmanna = len(self.employeeInfo)
@@ -145,22 +107,13 @@ class workplace:
             print(error)
         print()
 
+
     def print_employees(self):
+        teljari = 0
         for key, value in self.employeeInfo.items():
-            x = 0
-            for v in value:
-                x += 1
-                if x == 1:
-                    print("Employee ID", str(v) + ":")
-                elif x == 2:
-                    print(" First name: ", v)
-                elif x == 3:
-                    print(" Last name:", v)
-                elif x == 4:
-                    print(" Position:", v)
-                elif x == 5:
-                    print(" Salary:", v)
-            print()
+            for v, k in value.items():
+                print(" ", v + ":", k)
+
 
     def change_salary(self, id, salary=None, percentage=None):
 
@@ -170,63 +123,12 @@ class workplace:
             print(e)
 
     def change_employee(self, id, id_change=None, fyrsta_nafn_change=None, sidasta_nafn_change=None, starfsgrein_change=None, change_laun=None):
-        employeeInfo = self.employeeInfo
-        employeeInfoCopy = employeeInfo
-        x=0
-        for key, value in employeeInfoCopy.items():
-            x = x + 1
-            stop=False
-            if id == key[1]:
-                if id_change is not None:
-                    for key, value in employeeInfoCopy.items():
-                        if key[1] == id_change:
-                            print("Error: ID already exists. All changes canceled.")
-                            stop=True
-                        else:
-                            listi = list(key)
-                            listi[1] = id_change
-                            del employeeInfo[key]
-                            keyChange = tuple(listi)
-                            value[0] = id_change
-                            employeeInfo[keyChange] = value
-
-                if fyrsta_nafn_change is not None and stop is False:
-                    for key,value in employeeInfo:
-                        value[1] = fyrsta_nafn_change
-                        key[0] = value[1] + value[2]
-
-                if sidasta_nafn_change is not None and stop is False:
-                    for key, value in employeeInfo:
-                        value[2] = sidasta_nafn_change
-                        key = value[1] + value[2]
-
-                if starfsgrein_change is not None and stop is False:
-                    for key,value in employeeInfo:
-                        value[3] = starfsgrein_change
-
-                if change_laun is not None and stop is False:
-                    for key, value in employeeInfo:
-                        for k in key:
-                            if k == id:
-
-
-                        for k in key:
-
-                        employeeInfo[] = change_laun
-
-            else:
-                if x == len(employeeInfo):
-                    print("Error: input id does not match any id")
-        self.employeeInfo = employeeInfo
         print()
-
 
 x = workplace()
 x.create_employee("Daníel","Arnarsson","Forritari", 90000)
 x.create_employee("Ragnar", "Jónsson", "UI Designer", 85000)
 x.create_employee("Ragnar", "Jónsson", "UI Designer", 85000)
 x.create_employee("Sigurður", "andrason","HalloHallo",44444)
-x.search_employee(id = 2, name = "Ragnar Jónsson")
-x.delete_employee(id = 3, name = "Ragnar Jónsson")
-x.change_employee(id = 2, change_laun = 100000)
 x.print_employees()
+print(x.employeeInfo)
